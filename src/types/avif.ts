c004a27630eca21e28ab6188782efcc2cfaa0507
Pooya Parsa <pooya@pi0.io>
2024-07-03 10:43:35 +0200

    lint

--- src/types/avif.ts
+++ src/types/avif.ts
@@ -5,8 +5,13 @@ export const AVIF: IImage = {
   validate: (input) => toUTF8String(input, 8, 12) === "avif",
 
   calculate: (input) => {
-    function findBox(input: Uint8Array, type: string, startOffset = 0, endOffset = input.length) {
-      for (let offset = startOffset; offset < endOffset;) {
+    function findBox(
+      input: Uint8Array,
+      type: string,
+      startOffset = 0,
+      endOffset = input.length,
+    ) {
+      for (let offset = startOffset; offset < endOffset; ) {
         const size = readUInt32BE(input, offset);
         const boxType = toUTF8String(input, offset + 4, offset + 8);
 
@@ -23,14 +28,29 @@ export const AVIF: IImage = {
       throw new Error(`${type} box not found`);
     }
 
-    const metaBox = findBox(input, 'meta');
-    const iprpBox = findBox(input, 'iprp', metaBox.offset + 12, metaBox.offset + metaBox.size);
-    const ipcoBox = findBox(input, 'ipco', iprpBox.offset + 8, iprpBox.offset + iprpBox.size);
-    const ispeBox = findBox(input, 'ispe', ipcoBox.offset + 8, ipcoBox.offset + ipcoBox.size);
+    const metaBox = findBox(input, "meta");
+    const iprpBox = findBox(
+      input,
+      "iprp",
+      metaBox.offset + 12,
+      metaBox.offset + metaBox.size,
+    );
+    const ipcoBox = findBox(
+      input,
+      "ipco",
+      iprpBox.offset + 8,
+      iprpBox.offset + iprpBox.size,
+    );
+    const ispeBox = findBox(
+      input,
+      "ispe",
+      ipcoBox.offset + 8,
+      ipcoBox.offset + ipcoBox.size,
+    );
 
     const width = readUInt32BE(input, ispeBox.offset + 12);
     const height = readUInt32BE(input, ispeBox.offset + 16);
 
     return { width, height };
-  }
+  },
 };
